e235f6cd734239912640770a5c4f6c3f3538193f
Desmond Harris <desmond.fernando@nmg-group.com>
2024-01-18 09:19:19 -0500

    Added AAD and Authentication route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import logo from "./logo.svg"
-import { Counter } from "./features/counter/Counter"
 import "./App.css"
 import { QuestionMapping } from "./features/QuestionMapping/QuestionMapping"
 
@@ -7,50 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <Counter /> */}
+        <a href="/.auth/login/aad">Login</a>
+        <a href="/.auth/logout">Log out</a>
         <QuestionMapping />
-        {/* <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span> */}
       </header>
     </div>
   )
--- src/features/QuestionMapping/questionMappingAPI.ts
+++ src/features/QuestionMapping/questionMappingAPI.ts
@@ -3,14 +3,16 @@ import Question from "../model/Question"
 export function fetchAll(study: string) {
   return new Promise<{ data: Question[] }>((resolve) =>
     setTimeout(() => {
-      fetch("/data-api/rest/InsightsStudy").then(async (result: any) => {
+      fetch(
+        "/data-api/rest/InfoQuestion?$select=pkiQID,sQuestionID&$filter=fkiStudyID eq 24",
+      ).then(async (result: any): Promise<void> => {
         const quests = await result.json()
         console.table(quests.value)
         resolve({
           data: quests.value.map(
             // eslint-disable-next-line array-callback-return
-            (value: { pkiStudyID: number; sStudyName: string }) => {
-              return { QID: value.pkiStudyID, QuestionID: value.sStudyName }
+            (value: { pkiQID: number; sQuestionID: string }) => {
+              return { QID: value.pkiQID, QuestionID: value.sQuestionID }
             },
           ),
         })
